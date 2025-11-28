d61fc940f7012dd63d107f50a9b689e506df5a57
DavidBatkay <david.batkay@emanuel.ro>
2025-11-28 11:43:30 +0200

    Add share functionality to HomeScreen for sharing color names and hex values

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_4/models/css_color.dart';
 import 'package:flutter_application_4/screens/colored_screen.dart';
+import 'package:flutter_application_4/utils/color_utils.dart';
 import 'package:flutter_application_4/utils/utils.dart';
+import 'package:share_plus/share_plus.dart';
 
 import '../common/strings.dart' as strings;
 import '../data/css_colors.dart';
@@ -9,11 +12,22 @@ import '../widgets/css_color_list_item.dart';
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
+  void shareAll() {
+    final buffer = StringBuffer();
+    for (final color in cssColors) {
+      buffer.write('${color.colorName} ${toHex(color.color)} ');
+    }
+    SharePlus.instance.share(
+      ShareParams(text: buffer.toString()),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(strings.homeScreenTitle),
+        actions: [IconButton(onPressed: shareAll, icon: Icon(Icons.share))],
       ),
       body: ListView.builder(
         itemCount: cssColors.length,
